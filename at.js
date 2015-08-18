d8cea588bb2f992ec205e396adadd016e4e29ffa
FabioVergani <fabio.vergani.1975@gmail.com>
2015-08-18 19:39:30 +0200

    Update at.js

--- at.js
+++ at.js
@@ -3,12 +3,12 @@ console.clear();
 //
 function ready(e,s,o,f){console.dir(arguments);}//dispatcher,type,event,handler
 //
-function at(e,s,h){
+function at(e,s,h,b){
 	e.addEventListener(s,function f(x){
 	 var o=x,e=o.target,s=o.type;
 	 h(e,s,o,f);
 	 e.removeEventListener(s,f);
-	});
+	},b);
 };
 //
 at(document,'DOMContentLoaded',ready);
